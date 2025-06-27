303a36d7f1231061785c622b3bafa238c7d211d1
onore <jhonatansbestevao@gmail.com>
2025-06-27 12:58:25 -0300

    deploy firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:geoli/Uteis/constantes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'Uteis/ScrollBehaviorPersonalizado.dart';
-import 'firebase_options.dart';
 
 import 'Uteis/rotas.dart';
+import 'firebase_options.dart';
 
 void main() async {
   runApp(const MyApp());
